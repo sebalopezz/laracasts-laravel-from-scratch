afae6403209e42c3e069cc3b0932a6abcff3ba8c
sebalopezz <jorgesebastianlopez@gmail.com>
2019-06-28 19:11:19 -0300

    6-controllers

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,8 @@
 
 @section('content')
     
-    <h1>My first website </h1>
+    <h1>My first  {{ $foo }} website </h1>
 
-    <ul>
-        @foreach ($tasks as $task)
-            <li> {{ $task }}</li> 
-        @endforeach
-    </ul>
+
 
 @endsection
