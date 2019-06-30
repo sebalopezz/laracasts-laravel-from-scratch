e96b10d9f3743ae9885496039a48cab1ee7ff91d
sebalopezz <jorgesebastianlopez@gmail.com>
2019-06-30 19:33:46 -0300

    10-formcsrf

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1> Create a new project </h1>
+
+    <form action="/projects" method="POST">
+        {{ csrf_field() }}
+        <input type="text" name="title" placeholder="Project title"><br>
+        <textarea name="description" name="description" cols="25" rows="5"></textarea><br>
+        <button type="submit"> Create project </button>
+    </form>
+
+</body>
+</html>
